616b3d583aab385c286df9287cfdac9d62921e81
Thuanle27 <thuanle.qt@gmail.com>
2025-08-29 15:44:56 +0700

    thuanlv/Feat: Add CI

---
+++ sample_data/add_data.sh
@@ -0,0 +1,3 @@
+psql -h localhost -p 5434 -U postgres -d postgres -f Class.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f Course.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f Department.sql
