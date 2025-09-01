2c9a2e6f8c40d7ce0fb1bb44fcea5823a986613d
Thuanle27 <thuanle.qt@gmail.com>
2025-09-01 22:14:42 +0700

    thuanlv/Feat: have api the get current session of student

--- app/services/session_service.py
+++ app/services/session_service.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from app.models.session import Session as SessionModel
 from app.schemas.session import SessionCreate, SessionOut
-
+from datetime import datetime
 
 def get_all_session_service(db: Session) -> SessionOut:
     return db.query(SessionModel).all()
@@ -13,3 +13,15 @@ def create_session_service(db: Session, session_in: SessionCreate):
     db.commit()
     db.refresh(session)
     return session
+
+def get_current_session_service(db: Session, class_id: str) -> list[SessionOut]:
+    now = datetime.now()
+    return (
+        db.query(SessionModel)
+        .filter(
+            SessionModel.class_id == class_id,
+            SessionModel.start_time <= now,
+            SessionModel.end_time >= now,
+        )
+        .all()
+    )
