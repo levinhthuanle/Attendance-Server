39d6c89af6f6d10b04fa8d1ee70aae0ed5582484
Thuanle27 <thuanle.qt@gmail.com>
2025-09-05 18:50:24 +0700

    thuanlv/Feat: add another table name enrollment

---
+++ sample_data/Teacher.sql
@@ -0,0 +1,4 @@
+INSERT INTO "Teacher" (ID, user_id, teacher_id) VALUES
+    (103, 110, 'DL'),
+    (104, 111, 'HV'),
+    (105, 112, 'KH');
--- sample_data/add_data.sh
+++ sample_data/add_data.sh
@@ -1,3 +1,13 @@
-psql -h localhost -p 5434 -U postgres -d postgres -f Class.sql
-psql -h localhost -p 5434 -U postgres -d postgres -f Course.sql
+#!/bin/bash
+
+export PGPASSWORD="postgres"
 psql -h localhost -p 5434 -U postgres -d postgres -f Department.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f Course.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f User.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f Student.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f Teacher.sql
+# Create teacher with id DBT before running this
+psql -h localhost -p 5434 -U postgres -d postgres -f Class.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f Session.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f Record.sql
+psql -h localhost -p 5434 -U postgres -d postgres -f Enrollment.sql
