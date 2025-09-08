5a5730f3414e5c2e56d136245adf12ea9237f3b8
Thuanle27 <thuanle.qt@gmail.com>
2025-09-08 14:15:22 +0700

    thuanlv/Feat: Add get user profile api

--- app/api/users.py
+++ app/api/users.py
@@ -4,6 +4,7 @@ from app.db.deps import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserOut
 from app.services.user_service import create_user_service
+from app.core.security import get_current_user as get_current_user_dep
 
 router = APIRouter(prefix="/user", tags=["User"])
 
@@ -16,3 +17,9 @@ def create_user(user_in: UserCreate, db: Session = Depends(get_db)):
 @router.get("/", response_model=list[UserOut])
 def get_users(db: Session = Depends(get_db)):
     return db.query(User).all()
+
+
+
+@router.get("/current", response_model=UserOut)
+def get_current_user(current_user: User = Depends(get_current_user_dep)):
+    return current_user
